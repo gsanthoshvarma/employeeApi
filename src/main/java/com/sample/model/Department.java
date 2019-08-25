fd0faf0a5055c4d78e51358721a6876b6d67d92d
sandyG416 <sandeep.sm541@gmail.com>
2019-08-26 00:15:25 +0530

    created department class

---
+++ src/main/java/com/sample/model/Department.java
@@ -0,0 +1,37 @@
+package com.sample.model;
+
+public class Department {
+	
+	
+	private String deptName;
+	
+	private String deptId;
+	private String mangrId;
+	private String locId;
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	public String getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(String deptId) {
+		this.deptId = deptId;
+	}
+	public String getMangrId() {
+		return mangrId;
+	}
+	public void setMangrId(String mangrId) {
+		this.mangrId = mangrId;
+	}
+	public String getLocId() {
+		return locId;
+	}
+	public void setLocId(String locId) {
+		this.locId = locId;
+	}
+	
+
+}
