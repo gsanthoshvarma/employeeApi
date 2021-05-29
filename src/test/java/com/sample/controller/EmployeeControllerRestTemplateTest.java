840ab41c59204492b837760806438ca9d71a1987
gsanthoshvarma <santhoshvarma.g@gmail.com>
2021-05-30 01:34:33 +0530

    Modified Test class and removed uwanted spaces in pom

--- src/test/java/com/sample/controller/EmployeeControllerRestTemplateTest.java
+++ src/test/java/com/sample/controller/EmployeeControllerRestTemplateTest.java
@@ -1,21 +1,43 @@
 package com.sample.controller;
 
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 
 public class EmployeeControllerRestTemplateTest {
 	
 	private RestTemplate restTemplate = null;
+	private static final String AUTHENTICATE_REQUEST = "http://localhost:8080/employeeApi/authenticate/";
 	
 	public EmployeeControllerRestTemplateTest() {
 		this.restTemplate = new RestTemplate();
 	}
 	
 	@Test
-	public void getEmployeeByIdTest() {
-		String jwt = restTemplate.getForObject("http://localhost:8080/employeeApi/authenticate/", String.class);
-		System.out.println(jwt);
+	public void getEmployeeByIdTest() throws JsonProcessingException {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		MultiValueMap<String,String> requestMap = new LinkedMultiValueMap<>();
+			requestMap.add("username", "admin");
+			requestMap.add("password", "admin");
+			final HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<MultiValueMap<String, String>>(requestMap ,
+			        headers);
+		try {
+			ResponseEntity<String> responseEntity = restTemplate.postForEntity(AUTHENTICATE_REQUEST, entity, String.class);
+			System.out.println(responseEntity.getBody());
+		}catch (Exception e) {
+			System.out.println(e);
+		}
+		
 		
 	}
 	
